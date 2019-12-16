a1c7e565f4c376d3340248a65ea2072f548706f9
leokaka1 <leocaoxiaozhu@163.com>
2019-12-16 23:07:29 +0800

    添加了学习类的工程

--- Swift基础/Swift基础/ViewController.swift
+++ Swift基础/Swift基础/ViewController.swift
@@ -8,19 +8,42 @@
 
 import UIKit
 
+class Person: NSObject {
+    @objc var name:String = ""
+    @objc var age:Int = 0
+    @objc var sex: String = ""
+    
+    init(name:String,age:Int,sex:String = "男") {
+        self.name = name
+        self.age = age
+        self.sex = sex
+    }
+    
+    init(dict:[String:Any]) {
+        super.init()
+        setValuesForKeys(dict)
+    }
+    
+    func getPersonInfo() -> String{
+        let info = "我是\(name)，我今年\(age)岁了,我是一个\(sex)生"
+        return info
+    }
+    
+    override func setValue(_ value: Any?, forUndefinedKey key: String) {
+        
+    }
+}
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        variable()
+        
+        let P =  Person(name: "Leon", age: 19, sex: "男")
+        let P1 = Person(dict: ["name":"Leon","age":190,"sex":"man"])
+        print(P.getPersonInfo())
+        print(P1.getPersonInfo())
     }
 }
 
-extension ViewController{
-//    变量
-    func variable(){
-        var num1:Int = 1
-        num1 = 2
-        print(num1)
-    }
-}
+
